b2803df179c5a99630217f405726f67f72bd33ab
namminammi <southbeauty@gmail.com>
2016-01-07 15:54:02 -0500

    Waterproof refactor

--- LegoOnePage/LegoJs.js
+++ LegoOnePage/LegoJs.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
-  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top;
-
-   
-         $(this).click(function() {
-            $("#nav li a").removeClass("active");
-            $(this).addClass('active');
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           return false;
-         });
-      }
-    }
-  });
+    $('#nav li a').on('click', function(event) {
+        event.preventDefault();
+        var sectionID = $(this).attr("id");
+        scrollToID('#slide' + sectionID, 750);
+    });
 
 });
+
+
+function scrollToID(id, speed) {
+    var offSet = 50;
+    var targetOffset = $(id).offset().top - offSet;
+    $('html,body').animate({
+        scrollTop: targetOffset
+    }, speed);
+}
