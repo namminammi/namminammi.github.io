34446a9c6bf3ab3e70847fdcb9026183d9afc534
namminammi <southbeauty@gmail.com>
2016-01-07 15:59:09 -0500

    Waterproof scroll speed fix3

--- LegoOnePage/LegoJs.js
+++ LegoOnePage/LegoJs.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
-    $('#nav li a').on('click', function(event) {
-        event.preventDefault();
-        var sectionID = $(this).attr("id");
-        scrollToID('#slide' + sectionID, 900);
-    });
+$(document).ready(function(){
 
+ $('#nav li a').on('click', function(e) { 
+  var el = $( e.target.getAttribute('href') );
+  var elOffset = el.offset().top;
+  var elHeight = el.height();
+  var windowHeight = $(window).height();
+  var offset;
+
+  if (elHeight < windowHeight) {
+    offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+  }
+  else {
+    offset = elOffset;
+  }
+
+  var speed = 900;
+  $('html, body').animate({scrollTop:offset}, speed);
 });
 
-
-function scrollToID(id, speed) {
-    var offSet = 50;
-    var speed= 950;
-    var targetOffset = $(id).offset().top - offSet;
-    $('html,body').animate({
-        scrollTop: targetOffset
-    }, speed);
-}
+});
